b58b5a69582c235398835348730f9815d7455e77
Jean Nguyen <nguth464@school.lu>
2023-10-18 16:44:01 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Title: Block Game");
+        System.out.println("Project by Nguyen Jean and Majerus Mil");
+        System.out.println("CBL Project for 2IP90 in Eindhoven University of Technology");
+        
+    }
+}
